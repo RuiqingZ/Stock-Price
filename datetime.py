c48f44303f1c1acb5e019e0eac69267952981a4b
Ruiqing <ruiqing@RuiqingdeMacBook-Pro.local>
2019-08-07 15:05:22 -0400

    ready to merge

--- datetime.py
+++ datetime.py
@@ -1,17 +1,16 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
-Created on Tue Jul 16 15:31:46 2019
+Created on Tue Jul 23 16:48:17 2019
 
 @author: ruiqing
 """
-
 import quandl
+quandl.read_key()
 quandl.ApiConfig.api_key = quandl.ApiConfig.api_key
 
-
 def trading_signal(stiker):
-    data = quandl.get(stiker)
+    data = quandl.get('EOD/'+ stiker)
     # days represents how many days have been starting from the first day the program starts
     day = 1
     # A represents 250 days moving average while B represents 50 days moving average
@@ -32,3 +31,20 @@ def trading_signal(stiker):
         Result = 'Buying signal'
     print(Result)
     return Result
+
+from datetime import date
+today_num = date.today().weekday()
+days =["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]
+today = days[today_num]
+print(today)
+
+date_result = ""
+if today == "Saturday" or today == "Sunday":
+    date_result = "Holiday"
+else:
+    date_result = "Trading day"
+
+# entering the stiker name and check whether there is a trading signal
+stiker = input("Please enter the stock you want to check")
+if date_result == "Trading day":
+    trading_signal(stiker)
